6476c19f60182114bac54f046fb5a56552526b89
Theos Mariani <theos.mariani@etu.unice.fr>
2017-10-13 09:21:00 +0200

    Création de la classe Bambou

---
+++ src/main/java/Oka/Bamboo.java
@@ -0,0 +1,17 @@
+package Oka;
+
+public class Bamboo {
+    private String color;
+
+    public Bamboo(String color){
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
