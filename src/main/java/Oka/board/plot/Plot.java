ee89c4be85f5b51b86ca4bd80c6d18d138f2c6f5
Gregoire Peltier <gregoire.peltier@etu.unice.fr>
2017-10-14 20:09:35 +0200

    Ajout du package plot pour les class lié au plot

--- src/main/java/Oka/board/plot/Plot.java
+++ src/main/java/Oka/board/plot/Plot.java
@@ -1,11 +1,13 @@
-package Oka.board;
+package Oka.board.plot;
+
+import Oka.board.Cell;
 
 import java.awt.*;
 
 public class Plot extends Cell {
     String color = "Blue";// Provisoirement fixer a une couleur pour la release 1
     boolean isIrrigated;
-
+    PlotState state;
     public Plot(Point coords) {
 
         super(coords);
