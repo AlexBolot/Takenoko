0a46ea85b04910957aac0d0e56de890bfdcdb06f
Gregoire Peltier <gregoire.peltier@etu.unice.fr>
2017-10-14 15:33:47 +0200

    Ajout des test pour la cellule

---
+++ src/test/java/Oka/board/CellTest.java
@@ -0,0 +1,38 @@
+package Oka.board;
+
+import Oka.entities.Gardener;
+import Oka.entities.Panda;
+import junit.framework.TestCase;
+
+import java.awt.*;
+
+public class CellTest extends TestCase {
+    Cell center;
+    Cell side;
+
+    public void setUp() throws Exception {
+        Panda panda = Panda.getInstance();
+        panda.setCoords(new Point(0, 0));
+        Gardener gardener = Gardener.getInstance();
+        gardener.setCoords(new Point(0, 0));
+
+        center = new Cell(new Point(0, 0));
+        side = new Cell(new Point(1, 1));
+        super.setUp();
+    }
+
+    public void testGardenerIsHere() {
+        //Right test for gardener is here
+
+        assertTrue(center.GardenerIsHere());
+        assertFalse(side.GardenerIsHere());
+
+    }
+
+    public void testPandaIsHere() {
+        assertTrue(center.PandaIsHere());
+        assertFalse(side.PandaIsHere());
+    }
+
+
+}
