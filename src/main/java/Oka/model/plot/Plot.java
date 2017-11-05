301c1b3f6100ee3bcc8290abc22e0d676f346de3
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-11-05 18:26:17 +0100

    OKA-82 Ne plus irriguer automatiquement les plots

--- src/main/java/Oka/model/plot/Plot.java
+++ src/main/java/Oka/model/plot/Plot.java
@@ -2,7 +2,6 @@ package Oka.model.plot;
 
 import Oka.model.Bamboo;
 import Oka.model.Cell;
-import Oka.model.Enums;
 import Oka.model.Enums.Color;
 import Oka.model.plot.state.NeutralState;
 
@@ -37,14 +36,15 @@ public class Plot extends Cell
         super(new Point());
         this.color = color;
         this.state = state;
+
+        if (isIrrigated()) addBamboo();
     }
 
     public Plot (Point coords, Color color)
     {
         super(coords);
         this.color = color;
-        state = new NeutralState();
-        addBamboo();
+        this.state = new NeutralState();
     }
     //endregion
 
@@ -52,11 +52,6 @@ public class Plot extends Cell
     public ArrayList<Bamboo> getBamboo ()
     {
         return bamboo;
-    }
-
-    public void setBamboo (ArrayList<Bamboo> bamboo)
-    {
-        this.bamboo = bamboo;
     }
 
     public void setIsIrrigated (boolean bool)
@@ -69,16 +64,19 @@ public class Plot extends Cell
         return color;
     }
 
-    public NeutralState getState() {
+    public NeutralState getState ()
+    {
         return state;
     }
 
-    public boolean isIrrigated() {
-        return isIrrigated || state.getIsIrrigated();
+    public void setState (NeutralState state)
+    {
+        this.state = state;
     }
 
-    public void setState(NeutralState state){
-        this.state=state;
+    public boolean isIrrigated ()
+    {
+        return isIrrigated || state.getIsIrrigated();
     }
     //endregion
 
@@ -114,24 +112,6 @@ public class Plot extends Cell
         Plot plot = (Plot) obj;
 
         return this.color.equals(plot.color) && this.getCoords().equals(plot.getCoords());
-
-        //fixme Vraiment trop compliqué pour ce dont on a besoin :D
-
-        /*boolean equality;
-        equality = plot.bamboo.equals(this.bamboo);
-        equality = equality && plot.getCoords().equals(this.getCoords());
-
-        if (this.getColor() != null && plot.getColor() != null)
-        {
-
-            equality = equality && plot.getColor().equals(this.getColor());
-        }
-        else
-        {
-            equality = equality && this.getColor() == plot.getColor();
-        }
-
-        return equality;*/
     }
 
     @Override
